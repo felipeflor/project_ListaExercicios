fd9f91dccb0d38f52abe60e253833f2b1706ad09
Felipe <felipedossantosflor@gmail.com>
2022-12-09 18:11:35 -0300

    @FelipeFlor [Task: 7]

---
+++ 7/CalcDias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    internal class CalcDias
+    {
+        public int Dias { get; set; }
+        public int Meses { get; set; }
+        public int Anos { get; set; }
+
+        public virtual string ExibirDados()
+        {
+            int resultado = (Anos * 365) + (Meses * 30) + Dias;
+            return ($"Sua idade total em dias é: {resultado}");
+        }
+    }
+}
---
+++ 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using _7;
+
+namespace exercicio7{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcDias calcDias = new CalcDias();
+
+            Console.Write("Informe sua idade: ");
+            calcDias.Anos = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe os meses completos: ");
+            calcDias.Meses = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe dos dias: ");
+            calcDias.Dias = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(calcDias.ExibirDados());            
+
+            Console.ReadKey();
+        }
+    }
+}
