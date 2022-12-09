72e7cc66ead7de7728b5434e5982bd87feff4bc0
Felipe <felipedossantosflor@gmail.com>
2022-12-08 21:50:02 -0300

    @FelipeFlor [Task: 6]

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using _6;
+
+namespace exercicio6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo(); 
+            
+            Console.Write("Informe a base do retângulo: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a altura do retângulo: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(retangulo.ExibirDados());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6/Retangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    internal class Retangulo
+    {
+        public double Altura { get; set; }
+        public double Base { get; set; }
+
+        public virtual string ExibirDados()
+        {
+            double Area = Base * Altura;
+            return ($"Altura informada: {Altura}\nBase informada: {Base}\nÁrea do retângulo: {Area}");
+        }
+    }
+}
