7685fd1f43355c0c4c1252d00d7b83eaf2b5d9ad
Felipe <felipedossantosflor@gmail.com>
2022-12-08 21:17:31 -0300

    @FelipeFlor: [Task 5]

--- ListaExerciciosImpactaCSharp/Program.cs
+++ ListaExerciciosImpactaCSharp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace exercicio5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado;
+            Console.WriteLine("Informe um número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            resultado = num - 1;
+            Console.WriteLine($"Número digitado é: {num}, seu antecessor é: {resultado}");
+
+            Console.ReadKey();
+        }
+    }
+}
