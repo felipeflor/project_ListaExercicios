35cc164f51975f0692f0f49327eb458e28b54189
Felipe <felipedossantosflor@gmail.com>
2022-12-09 19:46:54 -0300

    @FelipeFlor [Task: Inicio ex 8]

---
+++ 8/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos
+brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total
+de eleitores.*/
+
+using _8;
+
+namespace exercicio8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Votacao votacao = new Votacao();
+
+            Console.WriteLine("Bem-vindo ao sistema de votação!");
+            Console.WriteLine("Por favor informe a quantidade de eleitores que votarão.");
+            votacao.Total = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < votacao.Total; i++)
+            {
+                Console.WriteLine("Digite 1 para Nulo | 2 para Branco | " +
+                    "3 para candidato A | 4 para candidato B");
+                int voto = Convert.ToInt32(Console.ReadLine());
+
+                switch (voto)
+                {
+                    case 1:
+                        // código 1
+                        break;
+                    case 2:
+                        // código 2
+                        break;
+                    case 3:
+                        // código 1
+                        break;
+                    case 4:
+                        // código 1
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8/Votacao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Votacao
+    {
+        public int Brancos { get; set; }
+        public int Nulos { get; set; }
+        public int Validos { get; set; }
+        public int Total { get; set; }
+
+        public virtual string Validacao()
+        {
+            string retorno;
+            int validacao = Brancos + Nulos + Validos;
+            while(Total >= validacao)
+            {
+                retorno = ("Não será possível efetuar votação. Excedido limite de eleitores informados!");
+                break;
+            }
+            return retorno = ("Obrigado por votar!");
+        }
+        /*public virtual string ExibirDados()
+        {
+            
+        }*/
+    }
+}
