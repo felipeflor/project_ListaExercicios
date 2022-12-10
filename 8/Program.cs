0a4851332453a23d4c368da417e0b427d9b78254
Felipe <felipedossantosflor@gmail.com>
2022-12-09 22:24:04 -0300

    @FelipeFlor [Task: 8]

--- 8/Program.cs
+++ 8/Program.cs
@@ -2,45 +2,49 @@
 brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total
 de eleitores.*/
 
-using _8;
-
 namespace exercicio8
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Votacao votacao = new Votacao();
+            int nulo = 0, branco = 0, valido = 0;
 
             Console.WriteLine("Bem-vindo ao sistema de votação!");
             Console.WriteLine("Por favor informe a quantidade de eleitores que votarão.");
-            votacao.Total = Convert.ToInt32(Console.ReadLine());
+            int total = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < votacao.Total; i++)
+            for (int i = 0; i < total; i++)
             {
                 Console.WriteLine("Digite 1 para Nulo | 2 para Branco | " +
                     "3 para candidato A | 4 para candidato B");
                 int voto = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Você escolheu a opção: {voto}");
 
-                switch (voto)
+                if(voto == 1)
                 {
-                    case 1:
-                        // código 1
-                        break;
-                    case 2:
-                        // código 2
-                        break;
-                    case 3:
-                        // código 1
-                        break;
-                    case 4:
-                        // código 1
-                        break;
-                    default:
-                        break;
+                    nulo = nulo + 1;
+                }else if(voto == 2)
+                {
+                    branco++;
+                }else if(voto == 3)
+                {
+                    valido++;
+                }else if(voto == 4)
+                {
+                    valido++;
                 }
 
             }
+
+            double porcentNulo, porcentBranco, porcentValido;
+            porcentNulo = (total * nulo) / 100;
+            porcentBranco = (total * branco) / 100;
+            porcentValido = (total * valido) / 100;
+
+            Console.Write($"Porcetagem de votos nulos: {porcentNulo}%\n" +
+                $"Porcetagem de votos brancos: {porcentBranco}%\n" +
+                $"Porcentagem de votos válidos: {porcentValido}%");
 
             Console.ReadKey();
         }
