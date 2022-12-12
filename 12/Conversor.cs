975b5c6b3858d47c8321e2ca0e35f88ea2673d90
Felipe <felipedossantosflor@gmail.com>
2022-12-12 17:43:38 -0300

    @FelipeFlor [Task: Conversor temperatura]

---
+++ 12/Conversor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    internal class Conversor
+    {
+        public double Celsius { get; set; }
+        public double Fahrenheit { get; set; }
+        public virtual double FpC()
+        {
+            Celsius = (Fahrenheit - 32) / 1.8;
+            return Celsius;
+        }
+
+        public virtual double CpF()
+        {
+            Fahrenheit = (Celsius * 1.8) + 32;
+            return Fahrenheit;
+        }
+    }
+}
--- 12/Program.cs
+++ 12/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _12;
+
+namespace exercicio12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversor conversor = new Conversor();
+
+            Console.WriteLine("Olá, bem vindo ao conversor de temperatura Fahrenheit Celsius!");
+            Console.WriteLine("Digite 1 para converter Fahrenheit para Celsius\n" +
+                "Digite 2 para converter Celsius para Fahrenheit");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                Console.Write("Informe temperatura em graus Fahrenheit: ");
+                conversor.Fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Temperatura convertida para {conversor.FpC()}ºC");
+
+            }else if(opcao == 2)
+            {
+                Console.Write("Informe temperatura em graus Celsius: ");
+                conversor.Celsius = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Temperatura convertida para {conversor.CpF()}ºF");
+
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
