0cee209c675f685a44144b3a5e70f41d04d99b69
Felipe <felipedossantosflor@gmail.com>
2022-12-12 17:12:36 -0300

    @FelipeFlor [Task: 11]

---
+++ 11/Calculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    internal class Calculo
+    {
+        public double Salario { get; set; }
+        public double ComissaoFixa { get; set; }
+        public double ComissaoVariavel { get; set; }
+        public int QdtVendas { get; set; }
+        public double Vendas { get; set; }
+        public virtual string CalculoSalario()
+        {
+            ComissaoFixa = ((Salario * 1.05) - Salario) * QdtVendas;
+            ComissaoVariavel = (Vendas * 1.05) - Salario;
+            double salarioFinal = Salario + ComissaoFixa + ComissaoVariavel;
+
+            return ($"-------------------------\n" +
+                $"Salário base: R${Salario}\nComissão fixa: R${ComissaoFixa}\n" +
+                $"Comissão variavel: R${ComissaoVariavel}\nSalario final: R${salarioFinal}\n" +
+                $"-------------------------"); ;
+        }
+
+    }
+}
---
+++ 11/Program.cs
@@ -0,0 +1,24 @@
+﻿using _11;
+
+namespace exercicio11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo calculo = new Calculo();
+
+            Console.Write("Informe salário fixo dos vendedores: ");
+            calculo.Salario = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a quantidade de unidades vendidas: ");
+            calculo.QdtVendas = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe valor em reais vendidos: ");
+            calculo.Vendas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(calculo.CalculoSalario());
+
+            Console.ReadKey();
+
+        }
+    }
+}
