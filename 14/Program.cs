4b84c2260c604d9f534645cb4f1e128983c07849
Felipe <felipedossantosflor@gmail.com>
2022-12-14 22:46:54 -0300

    @FelipeFlor [Task: 14]

---
+++ 14/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace exercicio14
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double num, dez;
+
+            Console.WriteLine("Informe um número para comparação: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            dez = 10;
+
+            if(num < dez)
+            {
+                Console.WriteLine("Valor informado é menor que 10!");
+            }
+            else if(num == dez)
+            {
+                Console.WriteLine("Valor informado é igual a 10!");
+            }
+            else
+            {
+                Console.WriteLine("Valor informado é maior que 10!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
