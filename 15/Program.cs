ff0f74b256392ed60362fac743a9cd89e40bbd43
Felipe <felipedossantosflor@gmail.com>
2022-12-14 22:53:32 -0300

    @FelipeFlor [Task: 15]

---
+++ 15/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicio15
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um valor para verificação: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            if(x >= 0)
+            {
+                Console.WriteLine("Número digitado é positivo");
+            }
+            else
+            {
+                Console.WriteLine("Número digitado é negativo");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
