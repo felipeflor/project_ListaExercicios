b14f17a44329250dadbc83fa8e55141218c61ab0
Felipe <felipedossantosflor@gmail.com>
2022-12-14 23:03:36 -0300

    @FelipeFlor [Task: 16]

---
+++ 16/Program.cs
@@ -0,0 +1,20 @@
+﻿decimal preco, total;
+int x;
+
+Console.Write("Informe a quantidade de maças a serem compradas: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+if (x < 12)
+{
+    preco = 1.30m;
+    total = preco * x;
+    Console.WriteLine($"Valor total: R${total}");
+}
+else
+{
+    preco = 1.00m;
+    total = preco * x;
+    Console.WriteLine($"Valor total: R${total}");
+}
+
+Console.ReadKey();
